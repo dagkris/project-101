60396f909d5945f1a0f95eaaac51b93725a93f84
Dag <dagjk1995@gmail.com>
2019-08-28 14:38:54 +0200

    myCoding

--- backend/service/src/main/java/no/acntech/project101/employee/service/EmployeeService.java
+++ backend/service/src/main/java/no/acntech/project101/employee/service/EmployeeService.java
@@ -3,26 +3,37 @@ package no.acntech.project101.employee.service;
 import java.util.List;
 import java.util.Optional;
 
+import no.acntech.project101.company.consumer.BrregRestClient;
 import no.acntech.project101.employee.Employee;
+import no.acntech.project101.employee.repository.EmployeeRepository;
 
 public class EmployeeService {
 
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(final EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     public Employee save(final Employee employee) {
         //TODO
-        return null;
+        return employeeRepository.save(employee);
     }
 
     public Optional<Employee> findById(final Long id) {
         //TODO
-        return null;
+        return employeeRepository.findById(id);
     }
 
     public List<Employee> findAll() {
         //TODO
-        return null;
+        return employeeRepository.findAll();
     }
 
     public void delete(final Long id) {
         //TODO
+        if(employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+        }
     }
 }
--- backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
+++ backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
@@ -1,11 +1,17 @@
 package no.acntech.project101.web.employee.resources;
 
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("employees")
 //TODO This is a REST controler and should receive request on path employees
 public class EmployeeResource {
 
@@ -13,27 +19,45 @@ public class EmployeeResource {
     public EmployeeResource() {
     }
 
+    @GetMapping
     public ResponseEntity<List<EmployeeDto>> findAll() {
+        EmployeeDto employeeDto = new EmployeeDto(12345L, "Dag Johnsrud", "Kristiansen",
+                LocalDate.of(1995, 06, 15), 1337L);
+        EmployeeDto bilal = new EmployeeDto(2345L, "Bilal", "Matchoman",
+                LocalDate.of(1995, 12, 6), 1338L);
+        final List<EmployeeDto> liste = new ArrayList<EmployeeDto>();
+        liste.add(employeeDto);
+        liste.add(bilal);
         //TODO create a GET endpoint find all employees in the database and return them
-        return null;
+        return ResponseEntity.ok(liste);
     }
 
+    @GetMapping("{id}")
     public ResponseEntity<EmployeeDto> findById() {
+        EmployeeDto employeeDto = new EmployeeDto(12345L, "Dag Johnsrud", "Kristiansen",
+                LocalDate.of(1995, 06, 15), 1337L);
         // TODO create a GET endpoint that fetches a spesific employee based on its ID
-        return null;
+        return ResponseEntity.ok (employeeDto);
     }
 
-    public ResponseEntity createEmployee() {
+    @PostMapping
+    public ResponseEntity createEmployee(@RequestBody final EmployeeDto employeedto) {
+        final URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(1337L)
+                .toUri();
         //TODO Create a POST endpoint that accepts an employeeDTO and saves it in the database
 
-        return null;
+        return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping
     public ResponseEntity deleteEmployee() {
         // TODO Create a DELETE endpoint that deletes a specific employee
         return null;
     }
 
+    @PatchMapping
     public ResponseEntity updateEmployee() {
         //TODO Create a PATCH endpoint that updates an employee with new values
         return null;
